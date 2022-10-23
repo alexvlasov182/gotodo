834a40d4a442c8dc6eb3fbc7ec1614b84128842e
Alex Vlasov <drumlife182@gmail.com>
2022-10-23 19:35:18 +0200

    Connected to database from application

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	_"github.com/lib/pq"
 	"github.com/alexvlasov182/gotodo"
 	"log"
 	"github.com/alexvlasov182/gotodo/pkg/handler"
 	"github.com/alexvlasov182/gotodo/pkg/repository"
 	"github.com/alexvlasov182/gotodo/pkg/service"
 	"github.com/spf13/viper"
+	"github.com/joho/godotenv"
+	"os"
 )
 
 func main()  {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error initializating configs: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
