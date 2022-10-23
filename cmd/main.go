681b78fcf4a4297166e85f2781580fb92cea0e97
Alex Vlasov <drumlife182@gmail.com>
2022-10-23 16:10:22 +0200

    worked with architecture

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,14 @@ import (
 	"github.com/alexvlasov182/gotodo"
 	"log"
 	"github.com/alexvlasov182/gotodo/pkg/handler"
+	"github.com/alexvlasov182/gotodo/pkg/repository"
+	"github.com/alexvlasov182/gotodo/pkg/service"
 )
 
 func main()  {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,8 +1,16 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/alexvlasov182/gotodo/pkg/service"
+)
 
 type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
